f63e89f6ef1fcbaab23698ae7b5a7f77aaeffbfd
pseudoyu <pseudoyu@connect.hku.hk>
2024-12-11 11:34:50 +0800

    chore: 20241101 update

--- main.go
+++ main.go
@@ -93,7 +93,7 @@ func findClosestBlockArweave(client arweave.Client, targetTimestamp int64) (int6
 }
 
 func main() {
-	targetTimestamp := int64(1717200000)
+	targetTimestamp := int64(1722470400)
 
 	// Load .env file
 	err := godotenv.Load()
